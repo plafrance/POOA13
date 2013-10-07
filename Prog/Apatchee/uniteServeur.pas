a2ffaff3d21e1a94698c8bebf83e41705360f78c
Patrick Lafrance <plafrance@crosemont.qc.ca>
2013-10-07 15:32:56 -0400

    Création des répertoires Analyse Prog et Tests

---
+++ Prog/Apatchee/uniteServeur.pas
@@ -0,0 +1,76 @@
+unit uniteServeur;
+
+interface
+
+uses
+  uniteConnexionHTTPServeur;
+  uniteProtocole;
+  uniteRequete;
+  uniteReponse;
+
+type
+
+  // Le serveur traite les requêtes du client
+  Serveur = class
+
+    // Le chemin par lequel les requêtes sont envoyées au serveur
+    // et les réponses sont retournées au client
+    laConnexion: ConnexionHTTPServeur;
+
+    // Le protocole par lequel les requêtes sont traitées
+    leProtocole: Protocole;
+
+    // Permet d'initialiser le serveur en créant la connexion et le protocole
+    // @param unPort le numéro du port sur lequel le serveur écoute les requêtes
+    procedure initialiser(unPort:Word);
+
+    // Démare le traitement des requêtes
+    procedure demarrer;
+
+  end;
+
+implementation
+
+
+  procedure Serveur.initialiser(unPort:Word);
+  begin
+    // Instanciation de l'objet ConnexionHTTPServeur
+    laConnexion := ConnexionHTTPServeur.create(unPort);
+
+    // Instanciation de l'objet Protocole
+    leProtocole := Protocole.create;
+
+    // Affichage d'un message pour confirmer l'initialisation du serveur
+    // FormatDateTime('c', now) permet d'afficher la date sous la forme jj/mm/aaaa hh:mm:ss
+    writeln(formatDateTime('c', now),' le serveur est demarré sur le port : ',unPort);
+  end;
+
+
+  procedure Serveur.demarrer;
+  var
+    uneRequete: Requete;
+    uneReponse: Reponse;
+  begin
+    // Boucler infiniment
+    while true do
+    begin
+      // Envoi de la requête
+      uneRequete := laConnexion.lireRequete;
+
+      // Message de confirmation de la réception de la requête
+      writeln(uneRequete.dateReception,' Requête reçue de ',uneRequete.adresseDemandeur);
+
+      // La reponse reçoit le traitement de la requête
+      uneReponse := leProtocole.traiterRequete(uneRequete);
+
+      // Envoi de la reponse de la requête reçu
+      laconnexion.ecrireReponse(uneReponse);
+
+      // Fermeture de la connexion
+      laConnexion.fermerConnexion;
+    end;
+  end;
+
+end.
+
+
