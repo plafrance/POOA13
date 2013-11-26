1819598af20d69401bbec39a84eaf7ef8b66dc94
Patrick <plafrance@crosemont.qc.ca>
2013-11-25 21:47:21 -0500

    Commit pour l'équipe de Prog Phase III partie 2.
    Ajout de l'option -j dans le programme principal
    Lecture d'un fichier HTML dans le Protocole
    Plusieurs petites corrections et ajouts de commentaires.

--- Prog/Vanessa/Vanessa.dpr
+++ Prog/Vanessa/Vanessa.dpr
@@ -5,93 +5,120 @@ program Vanessa;
 
 
 uses
-  SysUtils,
-  uniteServeur in 'uniteServeur.pas',
-  uniteRequete in 'uniteRequete.pas',
-  uniteProtocole in 'uniteProtocole.pas',
-  uniteReponse in 'uniteReponse.pas',
-  uniteConnexion in 'uniteConnexion.pas',
-  uniteConnexionHTTPServeur in 'uniteConnexionHTTPServeur.pas',
-  SocketUnit in 'SocketUnit.pas',
-  uniteConsigneur in 'uniteConsigneur.pas';
+   SysUtils,
+   uniteServeur in 'uniteServeur.pas',
+   uniteRequete in 'uniteRequete.pas',
+   uniteProtocole in 'uniteProtocole.pas',
+   uniteReponse in 'uniteReponse.pas',
+   uniteConnexion in 'uniteConnexion.pas',
+   uniteConnexionHTTPServeur in 'uniteConnexionHTTPServeur.pas',
+   SocketUnit in 'SocketUnit.pas',
+   uniteConsigneur in 'uniteConsigneur.pas';
 
 const
    nomServeur = 'Vanessa';
 var
-   entierWord,numeroDuPort:Word;
+   typeConversion,numeroDuPort:Word;
    i:Byte;
+   //erreurPos veut dire qu'il y a eu une exception(un erreur est survenu,ErreurPositive)
    errorPos:Integer;
-   validePort,valideRepertoire: String;
+   validePort,valideRepertoireDeBase,valideRepertoireJournaux: String;
    leServeur: Serveur;
    leConsigneur:Consigneur;
 begin
-    leServeur := nil;
-    i:=1;
-    entierWord:=0;
-    ValidePort:='80';
-    numeroDuPort:=80;
-    valideRepertoire:='c:/htdocs';
-    //Instanciation de l'objet consigneur qui consigne les erreurs et les connexion du serveur.
-    leConsigneur:=Consigneur.create;
+   i:=1;
+   //Port utilisé par défaut
+   ValidePort:='80';
+   //Repertoire de base utilisé par défaut
+   validerepertoiredebase:='C:\htdocs';
+   //Reperoire des fichiers log utilisé par défaut
+   valideRepertoireJournaux:='C:\journaux';
    // Validation de la configuration.
    // ParamCount retourne le nombre de paramètres passés au programme sur la ligne de commande.
-    while i<=paramCount do
-    begin
+
+   //vérifie dans la ligne de commande si l'utilisateur ne veut pas utiliser un numéro de port peronnalisé
+   while i<=paramCount do
+   begin
       if paramStr(i)='-p' then
       begin
-        validePort:=paramStr(i+1);
-        inc(i);
+         validePort:=paramStr(i+1);
+         inc(i);
       end
+      //vérifie dans la ligne de commande si l'utilisateur ne veut pas utiliser un chemin peronnalisé pour le repertoire journaux
+      else if paramStr(i)='-j' then
+      begin
+         valideRepertoireJournaux:= paramStr(i+1);
+         inc(i);
+      end
+      //vérifie dans la ligne de commande si l'utilisateur ne veut pas utiliser un repertoire de base peronnalisé
       else if paramStr(i)='-w' then
       begin
-        valideRepertoire:= paramStr(i+1);
-        inc(i);
+         validerepertoiredebase:= paramStr(i+1);
+         inc(i);
       end
       else
       begin
-        leConsigneur.consignerErreur(nomServeur,'Erreur sur les paramètres d''entrées, serveur non démarré');
-        halt;
+         writeln(nomServeur+'Erreur sur les paramètres d''entrées, serveur non démarré');
+         halt;
       end;
       inc(i);
-    end;
-   //Validation d'un entier.
-    val(validePort,entierWord,errorPos);
-    if (errorPos<>0) then
+   end;
+
+    //Validation de l'existence des fichiers journaux.
+    if not directoryExists (valideRepertoireJournaux) then
+
     begin
+      writeln('le répertoire des fichiers journaux est invalide ');
+      halt;
+    end;
+
+   //Instanciation de l'objet consigneur le plus tot possible,consigne les erreurs et les connexion du serveur.
+   leConsigneur:=Consigneur.create(valideRepertoireJournaux+'\');
+
+   //Validation d'un entier.
+   val(validePort,typeConversion,errorPos);
+   //si il n'y a pas d'Erreur erreurPos est en quelque sorte un boolean, et 0 veut dire qu'il n'y a pas d'Erreur , autre(qui ne peut que etre 1) veut dire qu'il y a une erreur.
+   if (errorPos<>0) then
+   begin
       leConsigneur.consignerErreur(nomServeur,'Erreur sur le type du numero de port');
       halt;
-    end
-    else
-    begin
-    //Validation de l'intervalle.
+   end
+   else
+   begin
+      //Validation de l'intervalle.
       if (strToInt(validePort)<1) or (strToInt(validePort)>65535)then
       begin
-        leConsigneur.consignerErreur(nomServeur,'Erreur, numéro de port hors intervalle');
-        halt;
+         leConsigneur.consignerErreur(nomServeur,'Erreur, numéro de port hors intervalle');
+         raise Exception.create('Le numéro de port est invalide');
+         halt;
       end;
-    numeroDuPort:=strToInt(validePort);
-    end;
-    //Validation de l'existence du répertoire.
-    if not directoryExists(valideRepertoire) then
-    begin
-      leConsigneur.consignerErreur(nomServeur,'Erreur, répertoire non existant');
-      halt;
-    end;
-    try
-    // Instanciation du serveur
-    // L'initialisation du serveur sur un numéro de port et un répertoire de base.
-    leServeur := Serveur.create(numeroDuPort,leConsigneur,valideRepertoire);
-    except on e: Exception do
-      begin
-        //Consigne l'erreur et le concatène avec le message d'erreur de l'objet Exception.
-        leConsigneur.consignerErreur(nomServeur,' Erreur d''entrée/sortie ' + e.Message);
-        halt;
-      end;
-    end;
+      numeroDuPort:=strToInt(validePort);
+   end;
 
+
+   //Validation de l'existence du répertoire.
+   if not directoryExists(validerepertoiredebase) then
+   begin
+      leConsigneur.consignerErreur(nomServeur,'Erreur, répertoire non existant');
+      raise Exception.create('Le répertoire source est invalide ou inexistant');
+      halt;
+   end;
+
+   try
+      // Instanciation du serveur
+      // L'initialisation du serveur sur un numéro de port et un répertoire de base.
+      leServeur := Serveur.create(numeroDuPort,leConsigneur,validerepertoiredebase);
+   except on e: Exception do
+   begin
+      //Consigne l'erreur et le concatène avec le message d'erreur de l'objet Exception.
+      leConsigneur.consignerErreur(nomServeur,' Erreur d''entrée/sortie ' + e.Message);
+      halt;
+   end;
+   end;
+
+   //On instancie le serveur 
    leServeur.demarrer;
 
-   readln;
 
 
 end.
--- Prog/Vanessa/uniteProtocole.pas
+++ Prog/Vanessa/uniteProtocole.pas
@@ -3,19 +3,19 @@ unit uniteProtocole;
 interface
 
 uses
-  SysUtils, uniteReponse, uniteRequete, uniteConsigneur;
+   SysUtils, uniteReponse, uniteRequete, uniteConsigneur;
 
 //Traite les requêtes HTTP et fournit une réponse appropriée selon l'état du serveur
 type
-  Protocole = class
-    private
+   Protocole = class
+   private
       //Le répertoire local qui contient tous les sites web de notre serveur
       repertoireDeBase:String;
 
       //Un consigneur permettant de consigner tous les messages
       leConsigneur:Consigneur;
 
-    public
+   public
       //La methode traiterRequete analyse la requete et renvoie le code approprié au fureteur.
       //Elle  reçoit la requete envoyée par le fureteur en paramètre et retourne un objet de type Reponse.
       //
@@ -27,12 +27,13 @@ type
 
       function traiterRequete(uneRequete:Requete):Reponse;
 
+
+
       //Crée un objet Protocole qui traite les requêtes HTTP
       //et fournit une réponse appropriée selon l'état du serveur.
       //
       //@param unRepertoireDeBase le répertoire qui contient tous les sites web de notre serveur
       //@param unConsigneur sert à consigner des messages
-
       constructor create(unRepertoireDeBase:String;unConsigneur:Consigneur);
 
       //Accesseur du répertoire de base
@@ -47,95 +48,144 @@ type
 
       procedure setRepertoireDeBase(unRepertoireDeBase:String);
 
-  end;
+   end;
 
 
 implementation
 
 function Protocole.traiterRequete(uneRequete:Requete):Reponse;
 var
-  uneReponse:Reponse;
-  uneAdresseDemandeur:String;
-  uneVersionProtocole:String;
-  unCodeReponse:Word;
-  unMessage:String;
-  uneReponseHtml:String;
-  stringTemporaire:String;
+   uneReponse:Reponse;
+   uneAdresseDemandeur:String;
+   uneVersionProtocole:String;
+   unCodeReponse:Word;
+   unMessage:String;
+   uneReponseHtml:String;
+   stringTemporaire:String;
+   unFichier:Textfile;
+   uneLigne:String;
 begin
 
-  //write('[', formatDateTime('c',uneRequete.getDateReception), ']',' ', uneRequete.getAdresseDemandeur,' ', uneRequete.getMethode,' ', uneRequete.getUrl,' ', uneRequete.getVersionProtocole);
+   //write('[', formatDateTime('c',uneRequete.getDateReception), ']',' ', uneRequete.getAdresseDemandeur,' ', uneRequete.getMethode,' ', uneRequete.getUrl,' ', uneRequete.getVersionProtocole);
 
-  leConsigneur.consigner('ProtocoleHTTP', uneRequete.getAdresseDemandeur+'-'+uneRequete.getMethode+' ( '+uneRequete.getVersionProtocole+' )');
+   leConsigneur.consigner('ProtocoleHTTP', uneRequete.getAdresseDemandeur+'-'+uneRequete.getMethode+' ( '+uneRequete.getVersionProtocole+' )');
 
-  //Initialisation des données pour l'objet Reponse qui retourne une erreur pour le moment.
+   //Initialisation des données pour l'objet Reponse qui retourne une erreur pour le moment.
 
-  uneAdresseDemandeur:=uneRequete.getAdresseDemandeur;
-  uneVersionProtocole:='HTTP/1.1';
-  unCodeReponse:=404;
-  unMessage:='L''URL introuvable';
-  uneReponseHTML:='L''URL ('+uneRequete.getUrl+') n''existe pas sur le serveur, veuillez verifier l''orthographe et reessayer';
+   uneAdresseDemandeur:=uneRequete.getAdresseDemandeur;
+   uneVersionProtocole:='HTTP/1.1';
+   unCodeReponse:=404;
+   unMessage:='L''URL introuvable';
+   uneReponseHTML:='L''URL ('+uneRequete.getUrl+') n''existe pas sur le serveur, veuillez verifier l''orthographe et reessayer';
 
-  //Vérification de la méthode
 
-  if (uneRequete.getMethode<>uppercase('GET')) then
-  begin
-    unCodeReponse:=501;
-    unMessage:='Methode incompatible';
-    uneReponseHTML:=('La methode '+uneRequete.getMethode+' n''est pas compatible');
-  end;
+   //Vérification de la méthode
+   //Reception de la requete
+   if fileExists(repertoireDeBase+uneRequete.getUrl)then
+   begin
+      // / ou \ a verifier
+      assignFile(unFichier,repertoireDeBase+uneRequete.getUrl);
+      try
+         reset(unFichier);
+        unCodeReponse:=200;
+        unMessage:='OK';
+        while not eof (unFichier) do
+          begin
+            try
+              readln(unFichier,uneLigne);
+            except on e :Exception do
+              begin
+                unCodeReponse:=500;
+                unMessage:='erreur interne du serveur';
+                leConsigneur.consignerErreur('unProtocoleHTTP','impossible de lire la page demandée');
+              end;
+            end;
+          uneReponseHtml:=uneReponseHtml+uneLigne+#13; //chr(13)
+        end;
+        close(unFichier);
+      except on e :Exception do
+      begin
+         unCodeReponse:=500;
+         unMessage:='erreur interne du serveur';
+         leConsigneur.consignerErreur('ProtocoleHTTP','impossible d''ouvrir la page demandée'+e.message);
+      end;
+      end;
+   end;
 
-  //Utilisation d'un subString pour vérifier HTTP/ seulement
-  
-  stringTemporaire:=copy(uneRequete.getVersionProtocole,1,5);
 
-  //Vérification de x.y c'est à dire la 6e et 8e lettre du String pour qu'il soit bien entre 0 et 9
-  //Sinon, lève une exception
+   if (uneRequete.getMethode<>uppercase('GET')) then
+   begin
+      unCodeReponse:=501;
+      unMessage:='Methode non implémentée';
+      uneReponseHTML:=('La methode '+uneRequete.getMethode+' n''est pas implémentée');
+   end;
 
-  if  (stringTemporaire = 'HTTP/') and
-      ((strToInt(uneRequete.getVersionProtocole[6]) >= 0) and
-      (strToInt(uneRequete.getVersionProtocole[6]) <= 9)) and
-      ((strToInt(uneRequete.getVersionProtocole[8]) >= 0) and
-      (strToInt(uneRequete.getVersionProtocole[8]) <=9)) and
-      (uneRequete.getVersionProtocole[7] = '.') and (length(uneRequete.getVersionProtocole) < 9) then
-  else
-    raise Exception.create('Requete HTTP invalide');
 
-  //Vérifie le protocole HTTP
 
-  if (uneRequete.getVersionProtocole <> 'HTTP/1.0') and (uneRequete.getVersionProtocole <> 'HTTP/1.1') then
-  begin
-    unCodeReponse:=505;
-    unMessage:='Version HTTP non supportee';
-    uneReponseHtml:=('Protocole '+uneRequete.getVersionProtocole+' incompatble avec le serveur');
-  end;
+   //Utilisation d'un subString pour vérifier HTTP/ seulement
 
-  //Créer l'objet réponse avec tous les paramètres plus haut
+   stringTemporaire:=copy(uneRequete.getVersionProtocole,1,5);
 
-  uneReponse:=Reponse.create(uneAdresseDemandeur,uneVersionProtocole,unCodeReponse,unMessage,uneReponseHtml);
+   //Vérification de x.y c'est à dire la 6e et 8e lettre du String pour qu'il soit bien entre 0 et 9
+   //Sinon, lève une exception
 
-  //Retourne l'objet réponse
+   if  (stringTemporaire <> 'HTTP/') or
+          ((uneRequete.getVersionProtocole[6] < '0')) or
+              ((uneRequete.getVersionProtocole[6] > '9')) or
+          ((uneRequete.getVersionProtocole[8] < '0')) or
+              ((uneRequete.getVersionProtocole[8] > '9')) or
+          (uneRequete.getVersionProtocole[7] <> '.') or (length(uneRequete.getVersionProtocole) <> 9) then
+   begin
+      raise Exception.create('Version HTTP incompatible');
+   end;
 
-  result:=uneReponse;
+   //Vérifie le protocole HTTP
 
-  //Utilise la méthode consignerErreur du consigneur
+   if (uneRequete.getVersionProtocole <> 'HTTP/1.0') and (uneRequete.getVersionProtocole <> 'HTTP/1.1') then
+   begin
+      unCodeReponse:=505;
+      unMessage:='Version HTTP' +uneRequete.getVersionProtocole +'non supportée';
+      uneReponseHtml:=('Protocole '+uneRequete.getVersionProtocole+' incompatble avec le serveur');
+   end;
 
-  leConsigneur.consignerErreur('ProtocoleHTTP', uneReponseHtml);
+   //Créer l'objet réponse avec tous les paramètres plus haut
+
+   uneReponse:=Reponse.create(uneAdresseDemandeur,uneVersionProtocole,unCodeReponse,unMessage,uneReponseHtml);
+   //Retourne l'objet réponse
+
+   result:=uneReponse;
+
+
+   //Case of pour ainsi consigner une seule fois dépendament de la variable unCodeReponse
+   Case unCodeReponse of
+   501:leConsigneur.consigner('ProtocoleHTTP','La méthode '+uneRequete.getMethode+' n''est pas implémentée');
+   505:leConsigneur.consigner('ProtocoleHTTP','Version HTTP'+uneRequete.getVersionProtocole+'incompatible avec le serveur');
+   404:leConsigneur.consigner('ProtocoleHTTP',uneReponse.getAdresseDemandeur + IntToStr(unCodeReponse) +':'+uneReponse.getMessage);
+   else leConsigneur.consignerErreur('ProtocoleHTTP','Erreur interne du serveur');
+   end;
 end;
+
+
 
 constructor Protocole.create(unRepertoireDeBase:String;unConsigneur:Consigneur);
 begin
-  setRepertoireDeBase(unRepertoireDeBase);
-  leConsigneur:=unConsigneur;
+   setRepertoireDeBase(unRepertoireDeBase);
+   leConsigneur:=unConsigneur;
+   //Verifie si le repertoire existe et affecte a la variable code le numero de code si le repertoire existe ou pas
+   if not directoryExists(unRepertoireDeBase)then
+    raise Exception.create('Repertoire de Base inexistant');
+
 end;
+
 
 function Protocole.getRepertoireDeBase:String;
 begin
-  result:=repertoireDeBase;
+   result:=repertoireDeBase;
 end;
 
 procedure Protocole.setRepertoireDeBase(unRepertoireDeBase:String);
 begin
-  repertoireDeBase:=unRepertoireDeBase;
+   repertoireDeBase:=unRepertoireDeBase;
 end;
 
 end.
